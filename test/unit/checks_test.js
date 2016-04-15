edcaa4275971dde726d54cf5aa308d0662f9a36b
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-04-15 14:47:13 +0000

    Test all checklist

---
+++ test/unit/checks_test.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect;
+var list = require('../../lib/list');
+var tester = require('../../lib/tester');
+
+describe('List', function () {
+  list.forEach(function (test) {
+    describe(test.description, function () {
+      test.tests.forEach(function (t) {
+        it('should pass valid output', function () {
+          expect(tester(t.expected, t.handler)).to.truthy;
+        });
+
+        it('should fail invalid output', function () {
+          expect(tester('', t.handler)).to.falsy;
+        });
+      });
+    });
+  });
+//   describe('Is the website only served over https?', function () {
+//     it('should pass valid output', function () {
+//       var test = list[0];
+//       expect(tester(test.tests[0].expected, test.tests[0].handler)).to.truthy;
+//     });
+    
+//     it('should fail invalid output', function () {
+//       var test = list[0];
+//       expect(tester('', test.tests[0].handler)).to.falsy;
+//     });
+//   });
+});
